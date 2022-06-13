4440c6c5f6d58d0b4547f786d46f2350d7784031
Javier Andres Olivera Soto <javierandresoliverasoto@gmail.com>
2022-06-13 11:03:33 -0500

    implementada el metodo de montar lista y metodo de contar proyectos

--- src/proyectosgrado/ProyectosGrado.java
+++ src/proyectosgrado/ProyectosGrado.java
@@ -4,17 +4,57 @@
  */
 package proyectosgrado;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author beraxes
  */
 public class ProyectosGrado {
 
-    /**
-     * @param args the command line arguments
-     */
+    proyecto inicio;
+    proyecto fin;
+
+    public ProyectosGrado() {
+        inicio = null;
+        fin = null;
+    }
+
+    public void montarLista() {
+        int cod;
+        String titu, aut, fecha;
+        cod = 0;
+        titu = JOptionPane.showInputDialog("Ingrese El Titulo Del Proyecto: ");
+        aut = JOptionPane.showInputDialog("Ingrese Los autores: ");
+        fecha = JOptionPane.showInputDialog("Ingrese la fecha de proyecto: ");
+        if (inicio == null) {
+            inicio = new proyecto();
+            inicio.registrarProyecto(cod + 1, titu, aut, fecha);
+            fin = inicio;
+        } else {
+            proyecto temp;
+            temp = new proyecto();
+            if (contarProyectos()< 100) {
+                temp.registrarProyecto(contarProyectos() + 1, titu, aut, fecha);
+            }else{
+                JOptionPane.showMessageDialog(null,"se ha excedido el numero de proyectos que se pueden registrar");
+            }
+        }
+    }
+
+    public int contarProyectos() {
+        proyecto temp;
+        temp = inicio;
+        int contador = 0;
+        while (temp != null) {
+            contador++;
+            temp = temp.getEnlace();
+        }
+        return contador;
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
